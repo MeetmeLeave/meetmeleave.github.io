655de22e95f5f118e781e3878ebf627fae366669
Vlad Martynenko <VMartynenko@lattice-engines.com>
2016-06-07 16:12:01 +0300

    generate rainshadowmap, generic canvas creation

--- src/renderer.js
+++ src/renderer.js
@@ -5,13 +5,56 @@ document.onreadystatechange = function() {
 }
 
 function generateMap() {
-    var c = document.getElementById("c");
+    generateAltitudeMap();
+    generateRainShadowMap();
+}
+
+function generateAltitudeMap() {
+    let c = getCanvasById("altitudeCanvas");
+
     mapgen.config.seed = parseInt(document.getElementById('seed').value);
-    mapgen.config.sizex = 700;
-    mapgen.config.sizey = 700;
+    mapgen.config.sizex = parseInt(document.getElementById('mapWidth').value);
+    mapgen.config.sizey = parseInt(document.getElementById('mapHeight').value);
     mapgen.config.removeBorders = !!document.getElementById('makeWater').checked;
     mapgen.config.scale = parseFloat(document.getElementById('perlinScale').value);
     mapgen.config.countOfNoiseNormalsTimes = parseInt(document.getElementById('noiseNormaliseCount').value);
     mapgen.createRandomIslandMap();
     mapgen.vizualizeMap(c, 'color');
+}
+
+function generateRainShadowMap() {
+    let c = getCanvasById("rainShadowCanvas");
+    $(c).addClass('canvasTopLayer');
+
+    mapgen.config.seed = parseInt(document.getElementById('seed').value);
+    mapgen.config.sizex = parseInt(document.getElementById('mapWidth').value);
+    mapgen.config.sizey = parseInt(document.getElementById('mapHeight').value);
+    mapgen.config.removeBorders = !!document.getElementById('makeWater').checked;
+    mapgen.config.scale = parseFloat(document.getElementById('perlinScale').value);
+    mapgen.config.countOfNoiseNormalsTimes = parseInt(document.getElementById('noiseNormaliseCount').value);
+    mapgen.createRandomIslandMap();
+    mapgen.vizualizeMap(c, 'grey');
+}
+
+function getCanvasById(id) {
+    let c = document.getElementById(id);
+
+    if (c === null) {
+        c = createNewCanvas(id);
+        $(c).addClass('canvas');
+        $('#island').append(c);
+    } else {
+        c.width = parseInt(document.getElementById('mapWidth').value);
+        c.height = parseInt(document.getElementById('mapHeight').value);
+    }
+
+    return c;
+}
+
+function createNewCanvas(id) {
+    let c = document.createElement("canvas");
+    c.width = parseInt(document.getElementById('mapWidth').value);
+    c.height = parseInt(document.getElementById('mapHeight').value);
+    c.id = id;
+    return c;
 }
